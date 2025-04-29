e4c382c420d22eb54b706b0acbb82fb8d2190966
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2025-04-29 21:53:05 +0300

    Update tmdb.ts

---
+++ src/services/tmdb.ts
@@ -0,0 +1,17 @@
+
+
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await axios.get(`${BASE_URL}/movie/popular`, {
+        params: {
+            api_key: API_KEY,
+            language: "en-US",
+            page: 1,
+        },
+    });
+    return response.data.results;
+};
