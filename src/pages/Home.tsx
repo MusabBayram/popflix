ef181043410c24d6434e3a7b99daaa64f712a406
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2025-05-04 17:00:08 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,39 @@
 import { useEffect, useState } from "react";
 import { getPopularMovies } from "../services/tmdb";
 import MovieCard from "../components/MovieCard";
+import { useLocation } from "react-router-dom";
 
 interface Movie {
   id: number;
   title: string;
   poster_path: string;
   vote_average: number;
+  genre_names?: string[];
 }
 
 const Home = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const showTopRated = params.get("topRated") === "true";
+  const selectedGenres = params.get("genres")?.split(",") || [];
 
   useEffect(() => {
     const fetchMovies = async () => {
       const data = await getPopularMovies();
-      setMovies(data);
+      const filtered = data.filter(
+        (movie) =>
+          (showTopRated ? movie.vote_average >= 7.5 : true) &&
+          (selectedGenres.length === 0 ||
+            selectedGenres.some((g) => movie.genre_names?.includes(g)))
+      );
+      setMovies(filtered);
     };
     fetchMovies();
-  }, []);
+  }, [showTopRated, selectedGenres]);
 
   return (
-    <div className="p-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 bg-white dark:bg-black">
+    <div className="min-h-screen p-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 bg-white dark:bg-black">
       {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
